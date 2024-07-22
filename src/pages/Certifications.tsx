6c29f25e984e8ebe62f81f9aa7bb35425b634301
Sneha Reddy <snehareddy9178@gmail.com>
2024-07-22 20:53:45 +0530

    add certifications page

---
+++ src/pages/Certifications.tsx
@@ -0,0 +1,14 @@
+import Title from '../utils/Title';
+
+function Certifications() {
+  return (
+    <div className="flex flex-col gap-8">
+      <div>
+        <Title title="COMPANY" />
+        <Title title="Certifications" isDark={true} />
+      </div>
+    </div>
+  );
+}
+
+export default Certifications;
