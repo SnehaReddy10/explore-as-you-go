f17f10cf24d7b68847b5993b447f51184ca99d73
Sneha Reddy <snehareddy9178@gmail.com>
2024-07-22 20:50:02 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      'gray-1': '#C8C8C8',
+      'gray-2': '#F3F3F3',
+      'gray-3': '#FBFBFB',
+      'gray-4': '#BDBDBD',
+      'gray-9': '#333333',
+      'gray-10': '#2C2C2C',
+      white: '#FFFFFF',
+    },
+  },
+  plugins: [],
+};
