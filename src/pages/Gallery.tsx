a4123aa0c0b838204bf546ea7e9e57351abdea3f
Sneha Reddy <snehareddy9178@gmail.com>
2024-07-22 20:53:21 +0530

    add gallery page

---
+++ src/pages/Gallery.tsx
@@ -0,0 +1,62 @@
+import Title from '../utils/Title';
+
+const photoGalleryImages = [
+  {
+    id: 6,
+    link: 'atrraction10',
+  },
+  {
+    id: 6,
+    link: 'attraction1',
+  },
+  {
+    id: 6,
+    link: 'attraction2',
+  },
+  {
+    id: 6,
+    link: 'attraction3',
+  },
+  {
+    id: 6,
+    link: 'atrraction15',
+  },
+  {
+    id: 6,
+    link: 'atrraction14',
+  },
+  {
+    id: 6,
+    link: 'atrraction13',
+  },
+  {
+    id: 6,
+    link: 'atrraction12',
+  },
+  {
+    id: 6,
+    link: 'atrraction11',
+  },
+  {
+    id: 6,
+    link: 'atrraction10',
+  },
+];
+
+function Gallery() {
+  return (
+    <div className="flex flex-col gap-8">
+      <div>
+        <Title title="Photo" />
+        <Title title="gallery" isDark={true} />
+      </div>
+      <div className="grid grid-cols-5 gap-2">
+        {photoGalleryImages.map((x) => (
+          <img src={`/assets/imgs/${x.link}.jpg`} className="h-72" />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Gallery;
